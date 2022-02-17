486ec0859066e3594183bcb6845d7844c5cd6f4e
MatteoRossi01 <93929073+MatteoRossi01@users.noreply.github.com>
2022-02-17 17:24:13 +0100

    Esercizio completato + bonus

--- main.js
+++ main.js
@@ -13,13 +13,12 @@ const app = new Vue (
         },
 
         mounted(){
-
-            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-            .then((response) => {
-                for(let i=0; i <10; i++){
+            for(let i=0; i <10; i++){
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then((response) => {
                     this.mail.push(response.data.response);
-                }
-            });
+                });      
+            }
         }
 
     }
